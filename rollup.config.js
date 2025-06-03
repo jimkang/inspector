55cced2e91aed503a7033bf0dcbcea6f5733ffb8
Jim Kang <jimkang@gmail.com>
2025-06-03 14:39:03 -0400

    Include the build.

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,17 @@ import node from "@rollup/plugin-node-resolve";
 import {terser} from "rollup-plugin-terser";
 import meta from "./package.json" assert {type: "json"};
 
-const copyright = `// @observablehq/inspector v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+const copyright = `// @observablehq/inspector v${meta.version} Copyright ${new Date().getFullYear()} Observable, Inc.`;
 
 export default [
   {
     input: "src/index.js",
-    plugins: [
-      node(),
-      terser({output: {preamble: copyright}})
-    ],
+    plugins: [node(), terser({output: {preamble: copyright}})],
     output: {
-      format: "umd",
+      format: "es",
       extend: true,
       name: "observablehq",
-      file: "dist/inspector.js"
-    }
-  }
+      file: "dist/inspector.js",
+    },
+  },
 ];
